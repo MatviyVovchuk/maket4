ef2a85f29649d84f3e2c7f01b827e3c06677ecd7
MatviyVovchuk <doc25900@gmail.com>
2024-01-24 18:01:24 +0200

    registration validation & smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ $(".menu-burger").on("click", function () {
 
 $(document).ready(function () {
   var screenWidth = $(window).width();
-  var $siteDevContent = $("#site-development-container .site-dev-content");
+  var $siteDevContent = $("#site_development_container .site-dev-content");
   var itemCount = $siteDevContent.find(".dev-content-item").length;
 
   function initializeSlider() {
@@ -37,7 +37,7 @@ $(document).ready(function () {
   }
 
   function destroySlider() {
-    $("#site-development-container .site-dev-content").slick("unslick");
+    $("#site_development_container .site-dev-content").slick("unslick");
   }
 
   // Initial setup
@@ -51,7 +51,7 @@ $(document).ready(function () {
 
     if (screenWidth < 850 || itemCount > 3) {
       if (
-        $("#site-development-container .site-dev-content").hasClass(
+        $("#site_development_container .site-dev-content").hasClass(
           "slick-initialized"
         )
       ) {
@@ -60,7 +60,7 @@ $(document).ready(function () {
       initializeSlider();
     } else {
       if (
-        $("#site-development-container .site-dev-content").hasClass(
+        $("#site_development_container .site-dev-content").hasClass(
           "slick-initialized"
         )
       ) {
@@ -74,7 +74,7 @@ $(document).ready(function () {
 /* SLIDER (Mentors) */
 
 $(document).ready(function () {
-  $("#student-feedback-container .feedback-content").slick({
+  $("#student_feedback_container .feedback-content").slick({
     slidesToShow: 1,
     arrows: false,
     dots: true,
@@ -88,7 +88,7 @@ $(document).ready(function () {
     cssEase: "linear",
   });
 
-  $("#mentors-slider-container .mentors-content").slick({
+  $("#mentors_slider_container .mentors-content").slick({
     slidesToShow: 1,
     arrows: false,
     dots: true,
@@ -97,4 +97,192 @@ $(document).ready(function () {
     infinite: true,
     initialSlide: 0,
   });
+});
+
+/* SMOOTH SCROLL */
+
+$(document).ready(function () {
+  // Smooth scroll to anchor links
+  $('a[href^="#"]').on("click", function (event) {
+    event.preventDefault();
+
+    var target = $(this.getAttribute("href"));
+
+    if (target.length) {
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top - $("header").outerHeight(),
+        },
+        1000
+      );
+    }
+  });
+});
+
+/* REGISTRATION FORM VALIDATION */
+
+$(document).ready(function () {
+  $("#field_phone").inputmask({ mask: "+38 (999) 999-99-99" });
+
+  // Form submit
+  $("#registration_form").on("submit", function (event) {
+    clearValidationStyles();
+
+    // Check all field together
+    let isFormValid = true;
+
+    if (!validateFirstName()) {
+      isFormValid = false;
+    }
+    if (!validateLastName()) {
+      isFormValid = false;
+    }
+    if (!validateIntresting()) {
+      isFormValid = false;
+    }
+    if (!validatePhone()) {
+      isFormValid = false;
+    }
+    if (!validateEmail()) {
+      isFormValid = false;
+    }
+    if (!validateMessage()) {
+      isFormValid = false;
+    }
+
+    if (!isFormValid) {
+      event.preventDefault();
+    }
+
+    // Check field one by one
+    // if (!validateName() || !validatePhone() || !validateEmail()) {
+    //   event.preventDefault();
+    //   console.log("NOT VALID");
+    // }
+
+    // Uncomment the line below to submit the form or remove event.preventDefault();
+    // $(this).unbind('submit').submit();
+    event.preventDefault();
+  });
+
+  function validateFirstName() {
+    const nameField = $("#field_firstname");
+    const nameValue = nameField.val().trim();
+
+    if (nameValue.length < 2 || nameValue.length > 15) {
+      setValidationError(
+        nameField,
+        "Ім'я повинно містити від 2 до 15 символів."
+      );
+      return false;
+    }
+
+    if (/\d/.test(nameValue)) {
+      setValidationError(nameField, "Ім'я не повинно містити цифри.");
+      return false;
+    }
+
+    return true;
+  }
+
+  function validateLastName() {
+    const nameField = $("#field_lastname");
+    const nameValue = nameField.val().trim();
+
+    if (nameValue.length < 2 || nameValue.length > 15) {
+      setValidationError(
+        nameField,
+        "Прізвище повинно містити від 2 до 15 символів."
+      );
+      return false;
+    }
+
+    if (/\d/.test(nameValue)) {
+      setValidationError(nameField, "Прізвище не повинно містити цифри.");
+      return false;
+    }
+
+    return true;
+  }
+
+  function validatePhone() {
+    const phoneField = $("#field_phone");
+    const phoneValue = phoneField.val().replace(/\D/g, ""); // Remove non-numeric characters
+
+    console.log(phoneField.val().replace(/\D/g, ""));
+
+    if (phoneValue === "") {
+      setValidationError(phoneField, "Введіть номер телефону.");
+      return false;
+    }
+
+    const phoneRegex = /^\d{12}$/; // Assumes a 10-digit Ukrainian phone number with 38 phone code
+    if (!phoneRegex.test(phoneValue)) {
+      setValidationError(phoneField, "Введіть правильний номер телефону.");
+      return false;
+    }
+
+    return true;
+  }
+
+  function validateIntresting() {
+    const interestingField = $("#field_interesting");
+    const interestingValue = interestingField.val().trim();
+
+    if (interestingValue.length < 2 || interestingValue.length > 25) {
+      setValidationError(
+        nameField,
+        "Поле повинно містити від 2 до 25 символів."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  function validateEmail() {
+    const emailField = $("#field_email");
+    const emailValue = emailField.val().trim();
+
+    if (emailField === "") {
+      setValidationError(phoneField, "Введіть адресу електронної пошти.");
+      return false;
+    }
+
+    if (!isValidEmail(emailValue)) {
+      setValidationError(
+        emailField,
+        "Введіть правильну адресу електронної пошти."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  function validateMessage() {
+    const messageField = $("#field_message");
+    const messageValue = messageField.val().trim();
+
+    if (messageValue === "") {
+      setValidationError(messageField, "Поле не може бути порожнім.");
+      return false;
+    }
+
+    return true;
+  }
+
+  function setValidationError(element, message) {
+    element.attr("placeholder", message).val(""); // Clear the value
+    element.addClass("error");
+  }
+
+  function clearValidationStyles() {
+    $("input, textarea").removeClass("error").attr("placeholder", "");
+  }
 });
